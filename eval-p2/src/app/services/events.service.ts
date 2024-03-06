fa33df3f82abf52687e8e4e58aae708210856505
Corentin Piccioli <corentin.piccioli@diiage.org>
2024-03-06 15:13:36 +0100

    Initial commit - Angular project setup

---
+++ eval-p2/src/app/services/events.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {HttpRequestService} from "../domain/http-request.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor() { }
+
+}
