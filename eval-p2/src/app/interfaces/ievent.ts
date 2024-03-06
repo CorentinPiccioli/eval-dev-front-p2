1fa4b5f1bca3eb87d93a2d0a67a377d545a46697
Corentin Piccioli <corentin.piccioli@diiage.org>
2024-03-06 15:29:33 +0100

    Setup environment variables and API services

---
+++ eval-p2/src/app/interfaces/ievent.ts
@@ -0,0 +1,6 @@
+export interface IEvent {
+  Id: string,
+  Description: string,
+  Date: Date,
+  Location: string,
+}
--- eval-p2/src/app/services/events.service.ts
+++ eval-p2/src/app/services/events.service.ts
@@ -1,11 +1,47 @@
 import { Injectable } from '@angular/core';
 import {HttpRequestService} from "../domain/http-request.service";
+import {IEvent} from "../interfaces/ievent";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private _httpRequest: HttpRequestService) { }
 
+  public getEvents() {
+    return new Promise<IEvent[]>((resolve, reject) => {
+      return this._httpRequest.get<any>('/event/list').subscribe({
+        next: result => resolve(result.Value),
+        error: error => reject(error)
+      });
+    });
+  }
+
+  public createEvent(event: IEvent) {
+    return new Promise<IEvent>((resolve, reject) => {
+      return this._httpRequest.post<any>('/event/add', event).subscribe({
+        next: result => resolve(result.Value),
+        error: error => reject(error)
+      });
+    });
+  }
+
+  public updateEvent(event: IEvent) {
+    return new Promise<IEvent>((resolve, reject) => {
+      return this._httpRequest.update<any>('/events', event).subscribe({
+        next: result => resolve(result.Value),
+        error: error => reject(error)
+      });
+    });
+  }
+
+  public deleteEvent(eventId: string) {
+    return new Promise<IEvent>((resolve, reject) => {
+      return this._httpRequest.delete<any>('/event/delete/'+eventId).subscribe({
+        next: result => resolve(result.Value),
+        error: error => reject(error)
+      });
+    });
+  }
 }
